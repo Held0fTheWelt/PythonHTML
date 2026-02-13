ebaf441ca9713085c577b9b88720c1ffc6ceeb32
Yves Tanas <Yves.Tanas@gmx.de>
2026-02-13 13:59:42 +0100

    first basic html response

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import requests
 
+def get_html(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    return response.status_code
+
+def get_input_url():
+    url = input("Enter URL: ")
+    return url
+
 def main():
-    pass
+    print(get_html(get_input_url()))
 
 
 if __name__ == "__main__":
