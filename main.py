feece455ca880e007a6e20ab0fddaaff378bf8ce
Yves Tanas <Yves.Tanas@gmx.de>
2026-02-13 14:15:33 +0100

    Added input and saving

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import requests
 
-def get_html(url):
+
+def save_to_file(text, file_name):
+    with open(file_name, "w", encoding="utf-8") as file:
+        file.write(text)
+
+def get_response(url):
     response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    return response.status_code
+    return response
+
+def get_filename():
+    filename = input("Enter the filename: ")
+    return filename
 
 def get_input_url():
     url = input("Enter URL: ")
     return url
 
 def main():
-    print(get_html(get_input_url()))
+    response = get_response(get_input_url())
 
+    if response.status_code != 200:
+        print(f"{response.status_code}: {response.text}")
+        return
+
+    save_to_file(response.text, get_filename())
 
 if __name__ == "__main__":
     main()
